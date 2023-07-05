6859b6a878eee2d0dc96f89f7d1885c250d1306b
juan sacasa <juanksacasa@gmail.com>
2023-07-05 10:14:43 -0600

    add files

--- javascript/main.js
+++ javascript/main.js
@@ -1,12 +1,17 @@
 import { buttonsEvents } from "./config/buttons.js";
 import { handleButtonClick } from "./modules/ids.js";
-import { renderEvents } from "./modules/render.js";
+import { handleCategoryChange, renderEvents } from "./modules/render.js";
+
 
 buttonsEvents();
 
+document.querySelectorAll('.nav-buttons').forEach(button => {
+  button.addEventListener('click', handleCategoryChange(button.dataset.category));
+});
+
 document.querySelectorAll(".nav-buttons").forEach(button => {
-    button.addEventListener("click", handleButtonClick);
-  });
+  button.addEventListener("click", handleButtonClick);
+});
 
 renderEvents('music');
 
